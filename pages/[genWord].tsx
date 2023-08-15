c9ddc2888599471b321447e5c143dd0b79e7b886
ComBba <cent@naver.com>
2023-08-15 18:48:30 +0900

    og TAG를 DB에서 적용되도록 수정.

--- pages/[genWord].tsx
+++ pages/[genWord].tsx
@@ -8,22 +8,37 @@ import Head from 'next/head';
 export default function Redirect() {
     const router = useRouter();
     const { genWord } = router.query;
-    const [originalUrl, setOriginalUrl] = useState('');
+    const [linkData, setLinkData] = useState({
+        original_url: '',
+        website_title: '',
+        website_locale: '',
+        website_image: '',
+        website_description: '',
+        site_name: ''
+    });
 
     useEffect(() => {
         async function redirect() {
             if (typeof genWord === 'string') {
                 const { data, error } = await supabase
                     .from('shortened_links')
-                    .select('original_url')
+                    .select('*')
                     .eq('genword', genWord)
-                    .single();
+                    .limit(1);
 
                 if (error) {
                     console.error('Error fetching original URL:', error);
-                } else if (data) {
-                    setOriginalUrl(data.original_url); // original_url을 상태로 저장
-                    window.location.href = data.original_url;
+                } else if (data && data.length > 0) {
+                    const link = data[0];
+                    setLinkData({
+                        original_url: link.original_url,
+                        website_title: link.website_title,
+                        website_locale: link.website_locale,
+                        website_image: link.website_image,
+                        website_description: link.website_description,
+                        site_name: link.site_name
+                    });
+                    window.location.href = link.original_url;
                 }
             }
         }
@@ -34,11 +49,13 @@ export default function Redirect() {
     return (
         <div>
             <Head>
-                <meta property="og:url" content={originalUrl} />
+                <meta property="og:url" content={linkData.original_url} />
                 <meta property="og:type" content="website" />
-                <meta property="og:title" content="Your Title Here" />
-                <meta property="og:description" content="Your Description Here" />
-                <meta property="og:image" content="https://olokeksbbvbugujymvaq.supabase.co/storage/v1/object/sign/avatars/4012c84b-865b-4b39-b9c4-4a9493453553-0.7932964515589191.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzLzQwMTJjODRiLTg2NWItNGIzOS1iOWM0LTRhOTQ5MzQ1MzU1My0wLjc5MzI5NjQ1MTU1ODkxOTEuanBlZyIsImlhdCI6MTY5MjA4Mzc3MCwiZXhwIjoxNjk0Njc1NzcwfQ.BBsAsKUlssD3P48p6NVLiwTzqDYBk5BGURizND0sS9I&t=2023-08-15T07%3A16%3A10.972Z" />
+                <meta property="og:title" content={linkData.website_title} />
+                <meta property="og:locale" content={linkData.website_locale} />
+                <meta property="og:image" content={linkData.website_image} />
+                <meta property="og:description" content={linkData.website_description} />
+                <meta property="og:site_name" content={linkData.site_name} />
             </Head>
             Redirecting...
         </div>
